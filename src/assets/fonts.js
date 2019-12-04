0f3d962f2eb40a52915f50d3c63b2853cc17e805
Esi're <esiri92@gmail.com>
2019-12-04 18:10:39 +0100

    Added assets
    Took 1 minute

---
+++ src/assets/fonts.js
@@ -0,0 +1,8 @@
+const fonts = {
+    neo_sans_pro_medium: 'neo_sans_pro_medium',
+    neo_sans_pro_regular: 'neo_sans_pro_regular',
+    roboto_bold: 'roboto_bold',
+    roboto_regular: 'roboto_regular'
+};
+
+export default fonts
---
+++ src/assets/images.js
@@ -0,0 +1,25 @@
+const images = {
+  'ic_arrow_right_128': require('./images/ic_arrow_right_128.png'),
+  'ic_bookmark_marked_128': require('./images/ic_bookmark_marked_128.png'),
+  'ic_clock_64': require('./images/ic_clock_64.png'),
+  'ic_comment_128': require('./images/ic_comment_128.png'),
+  'ic_hamburger': require('./images/ic_hamburger.png'),
+  'ic_home_128': require('./images/ic_home_128.png'),
+  'ic_instagram_128': require('./images/ic_instagram_128.png'),
+  'ic_logo': require('./images/ic_logo.png'),
+  'ic_menu_128': require('./images/ic_menu_128.png'),
+  'ic_menu_64': require('./images/ic_menu_64.png'),
+  'ic_notification': require('./images/ic_notification.png'),
+  'ic_privacy_128': require('./images/ic_privacy_128.png'),
+  'ic_round_facebook_128': require('./images/ic_round_facebook_128.png'),
+  'ic_round_twitter_128': require('./images/ic_round_twitter_128.png'),
+  'ic_round_youtube_128': require('./images/ic_round_youtube_128.png'),
+  'ic_search': require('./images/ic_search.png'),
+  'ic_settings_128': require('./images/ic_settings_128.png'),
+  'ic_share_128': require('./images/ic_share_128.png'),
+  'ic_terms_128': require('./images/ic_terms_128.png'),
+  'logo': require('./images/logo.png'),
+  'splash': require('./images/splash.png'),
+};
+
+export default images
