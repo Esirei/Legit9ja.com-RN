9a71aedb9c70b04d1e9454e1afe195f08dd4e1ac
Esi're <esiri92@gmail.com>
2019-12-09 23:22:04 +0100

    Updated PostScreen Content to navigate to new PostScreen on valid post link press in WebView.
    Took 21 minutes

--- src/screens/post/components/Content.tsx
+++ src/screens/post/components/Content.tsx
@@ -4,7 +4,9 @@ import { WebViewNavigation } from 'react-native-webview';
 import AutoHeightWebView from 'react-native-autoheight-webview';
 import YouTube from '@screens/post/components/YouTube';
 import { postContentWithoutYT } from '@helpers/post';
+import { NavigationService, RouteNames } from '@navigation';
 
+const postSlugRegex = /legit9ja.com\/[0-9]{4}\/[0-9]{2}\/(.*)\.html/;
 const css =
   '<style>body{width:100%;margin:0;text-align:start;}img {max-width:100%;height:auto;} iframe{width:100%;height:auto;}</style>';
 const Content = ({ post }) => {
@@ -12,7 +14,13 @@ const Content = ({ post }) => {
 
   const openExternalUrl = (webEvent: WebViewNavigation) => {
     if (webEvent.url) {
-      Linking.openURL(webEvent.url);
+      const matches = postSlugRegex.exec(webEvent.url);
+      if (matches) {
+        const post_slug = matches[1];
+        NavigationService.push(RouteNames.POSTS, { post_slug, source: 'slug' }, undefined);
+      } else {
+        Linking.openURL(webEvent.url);
+      }
       return false;
     }
     return true;
