2f3139a8588a243d55e410e28b84ca24bf22e990
Esi're <esiri92@gmail.com>
2019-12-09 02:38:09 +0100

    Updated image assets.
    Took 52 seconds

--- src/assets/images.js
+++ src/assets/images.js
@@ -1,8 +1,12 @@
 const images = {
   'ic_arrow_right_128': require('./images/ic_arrow_right_128.png'),
   'ic_bookmark_marked_128': require('./images/ic_bookmark_marked_128.png'),
+  'ic_bookmark_unmarked_128': require('./images/ic_bookmark_unmarked_128.png'),
+  'ic_chat_128': require('./images/ic_chat_128.png'),
+  'ic_chat_bubble_128': require('./images/ic_chat_bubble_128.png'),
   'ic_clock_64': require('./images/ic_clock_64.png'),
   'ic_comment_128': require('./images/ic_comment_128.png'),
+  'ic_email_128': require('./images/ic_email_128.png'),
   'ic_hamburger': require('./images/ic_hamburger.png'),
   'ic_home_128': require('./images/ic_home_128.png'),
   'ic_instagram_128': require('./images/ic_instagram_128.png'),
@@ -18,7 +22,9 @@ const images = {
   'ic_settings_128': require('./images/ic_settings_128.png'),
   'ic_share_128': require('./images/ic_share_128.png'),
   'ic_terms_128': require('./images/ic_terms_128.png'),
+  'ic_user_128': require('./images/ic_user_128.png'),
   'logo': require('./images/logo.png'),
+  'logo_androidv26': require('./images/logo_androidv26.png'),
   'splash': require('./images/splash.png'),
 };
 
