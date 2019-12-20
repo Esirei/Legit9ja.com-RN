013e58d483cb8e0eb943c9b3606653700a98de66
Esi're <esiri92@gmail.com>
2019-12-20 14:27:38 +0100

    Updated BannerAds component
    Took 56 minutes

--- src/components/Ads/BannerAds.tsx
+++ src/components/Ads/BannerAds.tsx
@@ -1,20 +1,22 @@
-import React, { memo } from 'react';
+import React, { memo, FC } from 'react';
 import { View, ViewStyle } from 'react-native';
-import { BannerAd, BannerAdSize } from '@react-native-firebase/admob';
+import { BannerAd, BannerAdSize, FirebaseAdMobTypes } from '@react-native-firebase/admob';
 
 interface Props {
   style?: ViewStyle;
+  size?: keyof FirebaseAdMobTypes.BannerAdSize;
 }
 
-const BannerAds = ({ style }: Props) => {
+const BannerAds: FC<Props> = ({ style, size }) => {
   return (
     <View style={style}>
-      <BannerAd
-        unitId={'ca-app-pub-7723042600622321/3183352336'}
-        size={BannerAdSize.SMART_BANNER}
-      />
+      <BannerAd unitId={'ca-app-pub-7723042600622321/3183352336'} size={size} />
     </View>
   );
+};
+
+BannerAds.defaultProps = {
+  size: BannerAdSize.SMART_BANNER,
 };
 
 export default memo(BannerAds);
