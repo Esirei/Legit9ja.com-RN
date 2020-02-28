7a9cc9d320b7a2073f727250d2718f5a2edd4cf1
Esi're <esiri92@gmail.com>
2020-02-28 23:03:54 +0100

    updated header helper.
    Took 49 minutes

--- src/helpers/header.ts
+++ src/helpers/header.ts
@@ -1,21 +1,25 @@
 import { Platform, StatusBar } from 'react-native';
 import { Header } from 'react-navigation-stack';
 
-// ios doesn't render a view for it's statusBar, so it's 0
-const RenderedStatusBarHeight = StatusBar.currentHeight || 0;
+// ios doesn't automatically render a view for it's statusBar, so it's 0
+const PlatformStatusBarHeight = StatusBar.currentHeight || 0;
 
+// Actual statusBar height perceived by user.
 // StatusBar height to be deducted if device has inset(notch) at the top, statusBar is most likely rendered in the inset.
-const RealStatusBarHeight = Platform.select({
-  android: RenderedStatusBarHeight,
+const RenderedStatusBarHeight = Platform.select({
+  android: PlatformStatusBarHeight,
   ios: 20, // Removing ios statusBar height from react-navigation's Header.Height getter
   default: 0,
 });
 
 // Header + StatusBar heights.
-// We hiding system rendered statusBar view in android, so need to add the height to rendered AppBar to emulate it.
-const DefaultAppBarHeight = Header.HEIGHT + RenderedStatusBarHeight;
+// If hiding system rendered statusBar view in android, might need to add the height to rendered Header to emulate it.
+// The Header.Height getter from react-navigation already contains rendered statusbar height for iOS
+const HeaderAndStatusBarHeight = Header.HEIGHT + PlatformStatusBarHeight;
 
-// AppBar height without status bar.
-const RealAppBarHeight = DefaultAppBarHeight - RealStatusBarHeight;
+// Header height excluding the status bar.
+const RenderedHeaderHeight = HeaderAndStatusBarHeight - RenderedStatusBarHeight;
 
-export const HeaderHeight = hasStatusBar => (hasStatusBar ? RealAppBarHeight : DefaultAppBarHeight);
+export const HeaderHeight = (hasStatusBar: boolean) => {
+  return hasStatusBar ? RenderedHeaderHeight : HeaderAndStatusBarHeight;
+};
